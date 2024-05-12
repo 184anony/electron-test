b9ac9a64dd35a654cad89cbd7573c9127a51889a
184anony <ochiken184@gmail.com>
2024-05-13 06:05:21 +0900

    カウントダウンタイマー機能を追加

--- renderer.js
+++ renderer.js
@@ -1,9 +1,33 @@
-const NOTIFICATION_TITLE = "Push通知のテストです！";
-const NOTIFICATION_BODY = "このように、Push通知を送信することができます！";
-const CLICK_MESSAGE = "Push通知がクリックされました！";
+const NOTIFICATION_TITLE = "Time's up!";
+const NOTIFICATION_BODY = "10秒経過しました";
+const CLICK_MESSAGE = "Time's up!";
 
 function sendNotification() {
     new Notification(NOTIFICATION_TITLE, {
         body: NOTIFICATION_BODY,
     }).onclick = () => console.log(CLICK_MESSAGE);
 }
+
+const countdown = document.getElementById("countdown");
+let seconds = 10;
+
+function updateCountdown() {
+    const minutes = Math.floor(seconds / 60);
+    let remainingSeconds = seconds % 60;
+
+    if (remainingSeconds < 10) {
+        remainingSeconds = "0" + remainingSeconds;
+    }
+
+    countdown.innerHTML = `${minutes}:${remainingSeconds}`;
+    if (seconds > 0) {
+        seconds--;
+    } else {
+        clearInterval(countdownInterval);
+        countdown.innerHTML = "Time's up!";
+        sendNotification();
+    }
+}
+
+updateCountdown();
+const countdownInterval = setInterval(updateCountdown, 1000);
