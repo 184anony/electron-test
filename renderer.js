93dc61fa6e94c212da1c19fd465f9c77e33d92aa
184anony <ochiken184@gmail.com>
2024-05-13 05:11:52 +0900

    通知機能を修正

--- renderer.js
+++ renderer.js
@@ -2,6 +2,8 @@ const NOTIFICATION_TITLE = "Push通知のテストです！";
 const NOTIFICATION_BODY = "このように、Push通知を送信することができます！";
 const CLICK_MESSAGE = "Push通知がクリックされました！";
 
-new window.Notification(NOTIFICATION_TITLE, {
-    body: NOTIFICATION_BODY,
-}).onclick = () => console.log(CLICK_MESSAGE);
+function sendNotification() {
+    new Notification(NOTIFICATION_TITLE, {
+        body: NOTIFICATION_BODY,
+    }).onclick = () => console.log(CLICK_MESSAGE);
+}
